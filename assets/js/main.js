d136e3275b555d29e611dc9a36da95de08e40307
mndlovu18 <mndlovu@brynmawr.edu>
2020-11-17 01:58:52 -0500

    about-page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Moderna - v2.1.0
-* Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
